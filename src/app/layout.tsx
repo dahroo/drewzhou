6cc518b737e67faa23f288498e5ce8f2afbcc1bc
drew <85300755+dahroo@users.noreply.github.com>
2024-09-06 04:00:54 -0400

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         {children}
     </div>
     <Footer />
+    <Analytics />
   </body>
 
 </html>
